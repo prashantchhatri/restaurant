202b339e8debcbde49be21ca5483961141afbc36
prashantchhatri <prashantchhatri2022@gmail.com>
2023-04-26 22:49:08 +0530

    react setup

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 heading = React.createElement("h1", {id: "heading"}, "Hello World From React!!");
 root = ReactDOM.createRoot(document.getElementById('main'));
 root.render(heading);
