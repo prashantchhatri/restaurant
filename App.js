e205622f0bce1a86eb129c41574105df3eaca994
prashantchhatri <prashantchhatri2022@gmail.com>
2023-04-29 15:06:51 +0530

    header component

--- App.js
+++ App.js
@@ -1,6 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-heading = React.createElement("h1", {id: "heading"}, "Hello World From React!!");
-root = ReactDOM.createRoot(document.getElementById('main'));
-root.render(heading);
+// heading = React.createElement("h1", {id: "heading"}, "Hello World From React!!");
+
+const Heading = () => {
+    return(
+        <div className="header">
+            <div className="">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSblZNHHBFmKTyJG0si_ISJ3ET6ABOj62CQ0g&usqp=CAU"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+    return(
+    <div className="app">
+        <Heading/>
+    </div>
+    );
+}
+
+
+
+
+
+root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout/>);
